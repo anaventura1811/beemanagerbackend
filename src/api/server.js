6a119e7e59f51a4f0d0b0ffc2b84951d59904936
Ana Ventura <anaventura1811@gmail.com>
2022-12-11 14:03:03 -0300

    feat/adiciona as rotas para o escopo de projects

--- src/api/server.js
+++ src/api/server.js
@@ -2,12 +2,18 @@ const express = require('express');
 const path = require('path');
 
 const bodyParser = require('body-parser');
+
 const userRoutes = require('../routes/users/userRoutes');
+const projectRoutes = require('../routes/projects/projectRoutes');
+const projectsRoutes = require('../routes/projects/projectsRoutes');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.use('/users', userRoutes);
+app.use('/project', projectRoutes);
+app.use('/projects', projectsRoutes);
+
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
--- src/routes/projects/projectsRoutes.js
+++ src/routes/projects/projectsRoutes.js
@@ -3,7 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 // inserir as validações depois
-router.post('/');
+
 router.get('/');
+router.put('/:id');
+router.patch('/:id/done');
+router.delete('/:id');
 
 module.exports = router;
